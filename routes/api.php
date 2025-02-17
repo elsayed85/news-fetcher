7d1c6c7e8d2ab2b40a3d330382f8feaddf034cd1
Elsayed Kamal <sayed.kamal@waseet.net>
2025-02-17 19:03:40 +0200

    add migrations and models for preferred categories, authors, and sources; implement user preference filtering

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\News\ArticleController;
 use App\Http\Controllers\News\AuthorController;
 use App\Http\Controllers\News\CategoryController;
 use App\Http\Controllers\News\SourceController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\ActAsUserMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
@@ -13,4 +15,12 @@ Route::prefix('v1')->group(function () {
     Route::get('/sources', [SourceController::class, 'index']);
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::get('/authors', [AuthorController::class, 'index']);
+
+    Route::prefix('user')
+        ->group(function () {
+            # for test purposes only
+            Route::get('/token', [UserController::class, 'token'])->middleware(ActAsUserMiddleware::class);
+
+            Route::get('/articles', [ArticleController::class, 'index'])->middleware('auth:sanctum');
+        });
 });
