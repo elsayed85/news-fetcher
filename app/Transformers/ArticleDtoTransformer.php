a4900b7a4a916771a7af9bb6099f843e869925a2
Elsayed Kamal <sayed.kamal@waseet.net>
2025-02-18 19:35:28 +0200

    feat: integrate CacheServiceInterface for caching in ArticleDtoTransformer

--- app/Transformers/ArticleDtoTransformer.php
+++ app/Transformers/ArticleDtoTransformer.php
@@ -2,6 +2,7 @@
 
 namespace App\Transformers;
 
+use App\Contracts\CacheServiceInterface;
 use App\Contracts\Repositories\AuthorRepositoryInterface;
 use App\Contracts\Repositories\CategoryRepositoryInterface;
 use App\Contracts\Repositories\SourceRepositoryInterface;
@@ -14,7 +15,8 @@ class ArticleDtoTransformer implements TransformerInterface
     public function __construct(
         protected SourceRepositoryInterface   $sourceRepository,
         protected AuthorRepositoryInterface   $authorRepository,
-        protected CategoryRepositoryInterface $categoryRepository
+        protected CategoryRepositoryInterface $categoryRepository,
+        protected CacheServiceInterface       $cacheService
     )
     {
     }
@@ -32,9 +34,18 @@ class ArticleDtoTransformer implements TransformerInterface
             url: $data->url,
             imageUrl: $data->imageUrl ?? null,
             publishedAt: $data->publishedAt,
-            sourceId: $this->sourceRepository->findOrCreate($data->getSource())->getKey(),
-            authorId: $this->authorRepository->findOrCreate($data->getAuthor())->getKey(),
-            categoryId: $this->categoryRepository->findOrCreate($data->getCategory())->getKey()
+            sourceId: $this->cacheService->rememberForever(
+                "source.{$data->getSource()}",
+                fn() => $this->sourceRepository->findOrCreate($data->getSource())->getKey()
+            ),
+            authorId: $this->cacheService->rememberForever(
+                "author.{$data->getAuthor()}",
+                fn() => $this->authorRepository->findOrCreate($data->getAuthor())->getKey()
+            ),
+            categoryId: $this->cacheService->rememberForever(
+                "category.{$data->getCategory()}",
+                fn() => $this->categoryRepository->findOrCreate($data->getCategory())->getKey()
+            )
         );
     }
 }
