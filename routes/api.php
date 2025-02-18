369c7fa995ce3f7243f0a21d1597b0781957bbcb
Elsayed Kamal <sayed.kamal@waseet.net>
2025-02-19 01:02:28 +0200

    refactor: remove unused UserController and ActAsUserMiddleware from API routes

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\News\ArticleController;
 use App\Http\Controllers\News\AuthorController;
 use App\Http\Controllers\News\CategoryController;
 use App\Http\Controllers\News\SourceController;
-use App\Http\Controllers\UserController;
-use App\Http\Middleware\ActAsUserMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
@@ -18,9 +16,6 @@ Route::prefix('v1')->group(function () {
 
     Route::prefix('user')
         ->group(function () {
-            # for test purposes only
-            Route::get('/token', [UserController::class, 'token'])->middleware(ActAsUserMiddleware::class);
-
             Route::get('/articles', [ArticleController::class, 'index'])->middleware('auth:sanctum');
         });
 });
